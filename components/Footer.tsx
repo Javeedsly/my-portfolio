711cdce70afda023acc8c74a30d03a58fcb5e62d
Javeedsly <cavidsly@gmail.com>
2025-11-02 00:16:13 +0400

    Deleted old files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center p-4 mt-10">
-      <p>© {new Date().getFullYear()} Bütün hüquqlar qorunur.</p>
+      <p>© {new Date().getFullYear()} All rights reserved.</p>
     </footer>
   );
 }
